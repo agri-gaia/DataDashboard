db9825cb7b754a2b6f3f8bbe655f4cad37a30ab1
julian-marco <julian.ende@lmis.de>
2023-06-12 09:01:39 +0200

    switch icon

--- src/modules/app/app-routing.module.ts
+++ src/modules/app/app-routing.module.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
   {
     path: 'meine-assets',
     component: CatalogBrowserComponent,
-    data: {title: 'Meine Assets', icon: 'sim_card' }
+    data: {title: 'Meine Assets', icon: 'home' }
   },
   {
     path: 'catalog-browser',
